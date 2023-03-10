4d1aea97136cfd0cc3f365d081d8d7a34e4ec654
feuvpi <fredvpg@gmail.com>
2023-03-09 23:47:31 -0300

    adding logic

---
+++ index.js
@@ -0,0 +1,5 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search');
+const weatherBox = document.querySelector('.weatherBox');
+const weatherDetails = document.querySelector('.weatherDetails');
+const notFound = document.querySelector('.notFound');
